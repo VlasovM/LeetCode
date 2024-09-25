b9277f0c4e600ca3b1672e28576df2276b838064
MaximVl <m.a.vlasov97@gmial.com>
2024-09-25 22:10:08 +0300

    [develop] Рефакторинг проекта:
    - Начал приводить в нужный порядок проект: добавил maven, добавил зависимости, слой тестов;
    - Перебрал задачку TwoSum и добавил для неё тесты;
    - Немного поправил gitignore

--- src/main/java/ru/javlasov/easy/TwoSum.java
+++ src/main/java/ru/javlasov/easy/TwoSum.java
@@ -1,26 +1,18 @@
-package TwoSum;
-
-import java.util.Arrays;
+package ru.javlasov.easy;
 
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * You can return the answer in any order.
+ * <p>
+ * https://leetcode.com/problems/two-sum/description/
  */
 
 public class TwoSum {
 
-    public static void main(String[] args) {
-        int[] nums = new int[]{2, 5, 5, 11};
-        int target = 10;
 
-        int[] result = twoSum(nums, target);
-        System.out.println(Arrays.toString(result));
-    }
-
-    public static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         int lengthArray = nums.length;
-
         for (int x = 0; x < lengthArray; x++) {
             int rightElement = nums[x];
             for (int y = x + 1; y < lengthArray; y++) {
