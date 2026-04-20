00b4e4e41f849cb5bfab4ae2fdde7283e4e88e85
JaVlasov <m.a.vlasov97@gmail.com>
2026-04-20 16:24:48 +0300

    [develop] Two Sum 2 - Input Array Is Sorted
    - Solve task use two-pointer method

---
+++ src/test/java/ru/javlasov/medium/TwoSum2InputArrayIsSortedTest.java
@@ -0,0 +1,55 @@
+package ru.javlasov.medium;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.javlasov.medium.TwoSum2InputArrayIsSorted.twoSum;
+
+class TwoSum2InputArrayIsSortedTest {
+
+    @Test
+    @DisplayName("Example 1: numbers = [2,7,11,15], target = 9")
+    void testRotateArrayTestExampleFirst() {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] expected = {1, 2};
+        System.out.println("expected: " + Arrays.toString(expected));
+
+        int[] actual = twoSum(numbers, target);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 2: numbers = [2,3,4], target = 6")
+    void testRotateArrayTestExampleSecond() {
+        int[] numbers = {2, 3, 4};
+        int target = 6;
+
+        int[] expected = {1, 3};
+        System.out.println("expected: " + Arrays.toString(expected));
+
+        int[] actual = twoSum(numbers, target);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 3: numbers = [-1,0], target = -1")
+    void testRotateArrayTestExampleThird() {
+        int[] numbers = {-1, 0};
+        int target = -1;
+
+        int[] expected = {1, 2};
+        System.out.println("expected: " + Arrays.toString(expected));
+
+        int[] actual = twoSum(numbers, target);
+
+        assertArrayEquals(expected, actual);
+    }
+
+}
