be11afe1c73c1367dd753d8e8f458c1cfd2bdcc5
JaVlasov <m.a.vlasov97@gmail.com>
2026-05-07 15:30:54 +0300

    [develop] Solve tasks: 12. Integer to Roman and 58. Length of Last Word

---
+++ src/test/java/ru/javlasov/medium/IntegerToRomanTest.java
@@ -0,0 +1,50 @@
+package ru.javlasov.medium;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.javlasov.medium.IntegerToRoman.intToRoman;
+
+class IntegerToRomanTest {
+
+    @Test
+    @DisplayName("Example 1: num = 3749")
+    void testExampleFirst() {
+        int num = 3749;
+
+        String expected = "MMMDCCXLIX";
+        System.out.println("expected: " + expected);
+
+        String actual = intToRoman(num);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 3: num = 58")
+    void testExampleSecond() {
+        int num = 58;
+
+        String expected = "LVIII";
+        System.out.println("expected: " + expected);
+
+        String actual = intToRoman(num);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 3: num = 1994")
+    void testExampleThird() {
+        int num = 1994;
+
+        String expected = "MCMXCIV";
+        System.out.println("expected: " + expected);
+
+        String actual = intToRoman(num);
+
+        assertEquals(expected, actual);
+    }
+
+}
