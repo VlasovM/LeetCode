a47a3dec425cbdb6f4c0b3d00d6934297cd60545
JaVlasov <m.a.vlasov97@gmail.com>
2026-05-13 12:09:54 +0300

    [develop] Solve 135. Candy

---
+++ src/test/java/ru/javlasov/hard/CandyTest.java
@@ -0,0 +1,37 @@
+package ru.javlasov.hard;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.javlasov.hard.Candy.candy;
+
+class CandyTest {
+
+    @Test
+    @DisplayName("Example 1: ratings = [1,0,2]")
+    void testExampleFirst() {
+        int[] ratings = {1, 0, 2};
+
+        int expected = 5;
+        System.out.println("expected: " + expected);
+
+        int actual = candy(ratings);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 2: ratings = [1,2,2]")
+    void testExampleSecond() {
+        int[] ratings = {1, 2, 2};
+
+        int expected = 4;
+        System.out.println("expected: " + expected);
+
+        int actual = candy(ratings);
+
+        assertEquals(expected, actual);
+    }
+
+}
