8fd250d49985a87006a8e4be73baf2c41992553f
JaVlasov <m.a.vlasov97@gmail.com>
2026-05-15 20:20:58 +0300

    [develop] Solve 28. Find the Index of the First Occurrence in a String

---
+++ src/test/java/ru/javlasov/easy/FindTheIndexOfTheFirstOccurrenceInAStringTest.java
@@ -0,0 +1,39 @@
+package ru.javlasov.easy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.javlasov.easy.FindTheIndexOfTheFirstOccurrenceInAString.strStr;
+
+class FindTheIndexOfTheFirstOccurrenceInAStringTest {
+
+    @Test
+    @DisplayName("Example 1: haystack = \"sadbutsad\", needle = \"sad\"")
+    void testExampleFirst() {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+
+        int expected = 0;
+        System.out.println("expected: " + expected);
+
+        int actual = strStr(haystack, needle);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 2: haystack = \"leetcode\", needle = \"leeto\"")
+    void testExampleSecond() {
+        String haystack = "leetcode";
+        String needle = "leeto";
+
+        int expected = -1;
+        System.out.println("expected: " + expected);
+
+        int actual = strStr(haystack, needle);
+
+        assertEquals(expected, actual);
+    }
+
+}
