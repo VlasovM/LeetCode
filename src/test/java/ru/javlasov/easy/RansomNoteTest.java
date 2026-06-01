4f16804dc8d38dbb03d4456da41248f6684808c6
JaVlasov <m.a.vlasov97@gmail.com>
2026-06-01 11:15:58 +0300

    [develop] Solve 383. Ransom Note

---
+++ src/test/java/ru/javlasov/easy/RansomNoteTest.java
@@ -0,0 +1,50 @@
+package ru.javlasov.easy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.javlasov.easy.RansomNote.canConstruct;
+
+class RansomNoteTest {
+
+    @Test
+    @DisplayName("Example 1: ransomNote = \"a\", magazine = \"b\"")
+    void testExampleFirst() {
+        String ransomNote = "a";
+        String magazine = "b";
+
+        boolean expected = false;
+
+        boolean actual = canConstruct(ransomNote, magazine);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 1: ransomNote = \"aa\", magazine = \"ab\"")
+    void testExampleSecond() {
+        String ransomNote = "aa";
+        String magazine = "ab";
+
+        boolean expected = false;
+
+        boolean actual = canConstruct(ransomNote, magazine);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 1: ransomNote = \"aa\", magazine = \"aab\"")
+    void testExampleThird() {
+        String ransomNote = "aa";
+        String magazine = "aab";
+
+        boolean expected = true;
+
+        boolean actual = canConstruct(ransomNote, magazine);
+
+        assertEquals(expected, actual);
+    }
+
+}
