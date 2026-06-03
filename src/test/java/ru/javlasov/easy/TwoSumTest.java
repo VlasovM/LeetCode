f47ae91fbb1413c43c89397480c867987142c856
JaVlasov <m.a.vlasov97@gmail.com>
2026-06-03 12:33:07 +0300

    [develop] Solve: 1. Two Sum, 242. Valid Anagram, 290. Word Pattern

---
+++ src/test/java/ru/javlasov/easy/TwoSumTest.java
@@ -0,0 +1,50 @@
+package ru.javlasov.easy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static ru.javlasov.easy.TwoSum.twoSum;
+
+class TwoSumTest {
+
+    @Test
+    @DisplayName("Example 1: nums = [2,7,11,15], target = 9")
+    void testExampleFirst() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] expected = {0, 1};
+
+        int[] actual = twoSum(nums, target);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 2: nums = [3,2,4], target = 6")
+    void testExampleSecond() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+
+        int[] expected = {1, 2};
+
+        int[] actual = twoSum(nums, target);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Example 3: nums = [3,3], target = 6")
+    void testExampleThird() {
+        int[] nums = {3, 3};
+        int target = 6;
+
+        int[] expected = {0, 1};
+
+        int[] actual = twoSum(nums, target);
+
+        assertArrayEquals(expected, actual);
+    }
+
+}
