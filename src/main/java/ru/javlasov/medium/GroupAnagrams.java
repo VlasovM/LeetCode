1630ed99fe69882d1e9c38beaed84ac103b23bc4
JaVlasov <m.a.vlasov97@gmail.com>
2026-06-04 10:32:04 +0300

    [develop] Solve: 49. Group Anagrams (with sorting

--- src/main/java/ru/javlasov/medium/GroupAnagrams.java
+++ src/main/java/ru/javlasov/medium/GroupAnagrams.java
@@ -1,9 +1,6 @@
 package ru.javlasov.medium;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * LeetCode:
@@ -13,33 +10,49 @@ import java.util.Map;
 public class GroupAnagrams {
 
     public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> countMap = new HashMap<>();
+        Map<String, List<String>> ans = new HashMap<>();
 
-        for (String word : strs) {
-            int[] countSymbolsArray = new int[26];
-
-            for (Character symbol : word.toCharArray()) {
-                countSymbolsArray[symbol - 'a']++;
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
             }
-
-            StringBuilder builder = new StringBuilder();
-            for (int count : countSymbolsArray) {
-                builder.append(count).append('#');
-            }
-
-            if (countMap.containsKey(builder.toString())) {
-                List<String> mapSubList = countMap.get(builder.toString());
-                mapSubList.add(word);
-            } else {
-                List<String> newMapSubList = new ArrayList<>();
-                newMapSubList.add(word);
-                countMap.put(builder.toString(), newMapSubList);
-            }
+            ans.get(key).add(s);
         }
 
-
-        return new ArrayList<>(countMap.values());
+        return new ArrayList<>(ans.values());
     }
+
+//    public static List<List<String>> groupAnagrams(String[] strs) {
+//        Map<String, List<String>> countMap = new HashMap<>();
+//
+//        for (String word : strs) {
+//            int[] countSymbolsArray = new int[26];
+//
+//            for (Character symbol : word.toCharArray()) {
+//                countSymbolsArray[symbol - 'a']++;
+//            }
+//
+//            StringBuilder builder = new StringBuilder();
+//            for (int count : countSymbolsArray) {
+//                builder.append(count).append('#');
+//            }
+//
+//            if (countMap.containsKey(builder.toString())) {
+//                List<String> mapSubList = countMap.get(builder.toString());
+//                mapSubList.add(word);
+//            } else {
+//                List<String> newMapSubList = new ArrayList<>();
+//                newMapSubList.add(word);
+//                countMap.put(builder.toString(), newMapSubList);
+//            }
+//        }
+//
+//
+//        return new ArrayList<>(countMap.values());
+//    }
 
 
 }
