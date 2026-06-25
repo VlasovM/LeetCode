647776c2fe66cbaf8e7a76931909d3790f6227eb
JaVlasov <m.a.vlasov97@gmail.com>
2026-06-25 15:57:51 +0300

    [develop] Solve 21. Merge Two Sorted Lists

---
+++ src/main/java/ru/javlasov/easy/LinkedListCycle.java
@@ -0,0 +1,40 @@
+package ru.javlasov.easy;
+
+import ru.javlasov.utills.ListNode;
+
+public class LinkedListCycle {
+
+    // time complexity is O(n + m) and memory complexity is O(1)
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.getVal() <= list2.getVal()) {
+                current.setNext(list1);
+                list1 = list1.getNext();
+            } else {
+                current.setNext(list2);
+                list2 = list2.getNext();
+            }
+
+            current = current.getNext();
+        }
+
+        if (list1 == null) {
+            current.setNext(list2);
+        } else {
+            current.setNext(list1);
+        }
+
+        return dummy.getNext();
+    }
+
+}
---
+++ src/test/java/ru/javlasov/easy/LinkedListCycleTest.java
@@ -0,0 +1,60 @@
+package ru.javlasov.easy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.javlasov.utills.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static ru.javlasov.easy.LinkedListCycle.mergeTwoLists;
+
+class LinkedListCycleTest {
+
+    private int[] listToArray(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.getVal());
+            head = head.getNext();
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    @Test
+    @DisplayName("Example 1: list1 = [1,2,4], list2 = [1,3,4]")
+    void testExampleFirst() {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        int[] expected = {1, 1, 2, 3, 4, 4};
+
+        ListNode actual = mergeTwoLists(list1, list2);
+
+        assertArrayEquals(expected, listToArray(actual));
+    }
+
+    @Test
+    @DisplayName("Example 2: list1 = [], list2 = []")
+    void testExampleSecond() {
+        ListNode list1 = null;
+        ListNode list2 = null;
+        ListNode merged = mergeTwoLists(list1, list2);
+        assertNull(merged);
+    }
+
+    @Test
+    @DisplayName("Example 3: list1 = [], list2 = [0]")
+    void testExampleThird() {
+        ListNode list1 = null;
+        ListNode list2 = new ListNode(0);
+
+        int[] expected = {0};
+
+        ListNode actual = mergeTwoLists(list1, list2);
+
+        assertArrayEquals(expected, listToArray(actual));
+    }
+
+}
